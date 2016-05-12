6e780d8184746557660e22bd6026c455509f2e79
jotase <jota.segovia@gmail.com>
2016-05-12 18:07:11 -0300

    Agregado funciones lider, reglas, weon malo

--- app.rb
+++ app.rb
@@ -12,6 +12,8 @@ post '/gateway' do
     case action
     when 'tabla' then table
     when 'lider' then leader
+    when 'reglas' then rules
+    when 'weon malo' then loser
     else respond_message 'Accion desconocida'
     end
   else
@@ -26,15 +28,20 @@ def respond_message(message)
 end
 
 def table
+  get_table 'team'
+  message = ([@headers] + @rows).map { |r| r.join ', ' }.join "\n"
+  respond_message message
+end
+
+def get_table(type)
   page = table_page
-  table = page.search('.//table').first
-  headers = table.search('.//th').map(&:text)
+  table = page.search('.//table')
+  table = type == 'team' ? table.first : table.last
+  @headers = table.search('.//th').map(&:text)
   trs = table.search('.//tbody//tr')
-  rows = trs.map do |tr|
+  @rows = trs.map do |tr|
     tr.search('.//td').map(&:text)
   end
-  message = ([headers] + rows).map { |r| r.join ', ' }.join "\n"
-  respond_message message
 end
 
 def table_page
@@ -42,6 +49,31 @@ def table_page
   @agent.get 'http://www.fifagenerator.com/tournament/47617/table/'
 end
 
+def leader
+  get_table 'player'
+  respond_message "#{@rows.first[1]} con #{@rows.first[9]} puntos"
+end
+
+def loser
+  get_table 'player'
+  respond_message "#{@rows.last[1]} con #{@rows.first[9]} puntos WEON MALO"
+end
+
 def test(params, message)
   respond_message "params: #{params} | message: #{message}"
+end
+
+def rules
+  _rules = '1. Partidos de 5 Minutos',
+           '2. Todos contra todos',
+           '3. Ida y vuelta',
+           '4. Clasifican los primeros 4',
+           '5. Partidos a las 13:00 hrs y 19:00 (en adelante)',
+           '6. Se respeta el orden del fixture (de no poder uno, se corre al siguiente partido)',
+           '7. Los 2 últimos pagan las pizzas de la final',
+           '8. Los 3 primeros lugares reciben (30.00 - 15.000 y 5.000 respectivamente)',
+           '9. Se pide respeto del público presente, juego limpio',
+           '10. Están permitidos los bombos, los lienzos, el fuego de artificio y porristas.'
+  message =  _rules.join "\n"
+  respond_message message
 end
